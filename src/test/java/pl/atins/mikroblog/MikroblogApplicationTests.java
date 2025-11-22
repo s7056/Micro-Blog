f4f29f4dca277a715f1cca6319442225e39099a3
Tagrun <yssboern@gmail.com>
2025-11-22 14:33:55 +0200

    spring boot project scaffolding

---
+++ src/test/java/pl/atins/mikroblog/MikroblogApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.atins.mikroblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikroblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
